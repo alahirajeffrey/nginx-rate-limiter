7df5b4c2263aeb78993201fa1ec3318b0af5ad09
alahirajeffrey <alahirajeffrey@gmail.com>
2024-05-21 10:54:38 +0100

    updated route

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const port = 3000;
 
 const app = express();
 
-app.get("/nginx-test", (req, res) => {
-  return res.status(200).json({ message: "testing nginx rate limiter" });
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "hello world" });
 });
 
 app.listen(port, () => {
