af73ab13d1f8d72fa9b484e5de3ce93aa39046dc
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-02 19:04:42 +0100

    setup cors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const cors = require("cors");
 const port = 3000;
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "hello world" });
